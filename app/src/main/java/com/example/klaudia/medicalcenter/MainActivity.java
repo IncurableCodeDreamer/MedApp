21e2a6866dffd5f705f2e3c1342ad9a8a2a96893
IncurableCodeDreamer <kla.96.kn@gmail.com>
2018-03-04 20:08:25 +0100

    Create Nav Acivity

--- app/src/main/java/com/example/klaudia/medicalcenter/MainActivity.java
+++ app/src/main/java/com/example/klaudia/medicalcenter/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.klaudia.medicalcenter;
 
+import android.annotation.SuppressLint;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle Toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.draweLayout);
+        Toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(Toggle);
+        Toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+            public boolean onOptionsItemSelected(MenuItem item){
+
+        if(Toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
