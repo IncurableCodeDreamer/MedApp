065d201a1f1973610a3cdc310344c8e889b36210
IncurableCodeDreamer <kla.96.kn@gmail.com>
2018-10-22 19:21:04 +0200

    Edit user image feature

--- app/src/main/java/com/example/klaudia/medicalcenter/AddAccountActivity.java
+++ app/src/main/java/com/example/klaudia/medicalcenter/AddAccountActivity.java
@@ -1,15 +1,16 @@
 package com.example.klaudia.medicalcenter;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,6 +20,9 @@ import java.util.List;
 import butterknife.BindView;
 
 public class AddAccountActivity extends AppCompatActivity{
+
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,35 +46,62 @@ public class AddAccountActivity extends AppCompatActivity{
             @SuppressLint("WrongConstant")
             @Override
             public void onClick(View v) {
-                List<Account> accountList = new ArrayList<>();
-                LinearLayout layout = findViewById(R.id.add_account_layout);
-
-                for (int i = 0; i < layout.getChildCount(); i++) {
-                    View child = layout.getChildAt(i);
-
-                    if (child instanceof EditText) {
-                        String childValue = ((EditText) child).getText().toString();
-
-                        if (childValue.length() != 0) {
-                            String childName = ((TextView) layout.getChildAt(i - 1)).getText().toString();
-                            Account account = new Account();
-                            account.setName(childName);
-                            account.setValue(childValue);
-                            accountList.add(account);
-                        }
-                    }
-                }
-
-                CheckBox ifDonor = findViewById(R.id.ifDonor_checkbox);
-                if (ifDonor.isChecked()) {
-                    AccountActivity.ifChecked = true;
-                } else {
-                    AccountActivity.ifChecked = false;
-                }
-
-                Intent intent = new Intent(AddAccountActivity.this, AccountActivity.class);
-                startActivity(intent);
+                saveAccount();
             }
         });
+
+        ImageView image = findViewById(R.id.nav_image);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operGallery();
+            }
+        });
+    }
+
+    private void saveAccount() {
+        List<Account> accountList = new ArrayList<>();
+        LinearLayout layout = findViewById(R.id.add_account_layout);
+
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View child = layout.getChildAt(i);
+
+            if (child instanceof EditText) {
+                String childValue = ((EditText) child).getText().toString();
+
+                if (childValue.length() != 0) {
+                    String childName = ((TextView) layout.getChildAt(i - 1)).getText().toString();
+                    Account account = new Account();
+                    account.setName(childName);
+                    account.setValue(childValue);
+                    accountList.add(account);
+                }
+            }
+        }
+
+        CheckBox ifDonor = findViewById(R.id.ifDonor_checkbox);
+        if (ifDonor.isChecked()) {
+            AccountActivity.ifChecked = true;
+        } else {
+            AccountActivity.ifChecked = false;
+        }
+
+        Intent intent = new Intent(AddAccountActivity.this, AccountActivity.class);
+        startActivity(intent);
+    }
+
+    private void operGallery() {
+        Intent gallery = new Intent(getIntent().ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            ImageView image = findViewById(R.id.nav_image);
+            imageUri = data.getData();
+            image.setImageURI(imageUri);
+        }
     }
 }
--- app/src/main/java/com/example/klaudia/medicalcenter/MainActivity.java
+++ app/src/main/java/com/example/klaudia/medicalcenter/MainActivity.java
@@ -85,18 +85,7 @@ public class MainActivity extends AppCompatActivity {
         userBirth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(MainActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener,
-                        year, month, day);
-
-                dialog.getDatePicker().setMaxDate(System.currentTimeMillis());
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
+                createDateDialog();
             }
         });
 
@@ -110,6 +99,21 @@ public class MainActivity extends AppCompatActivity {
                 userBirth.setText(date);
             }
         };
+    }
+
+    private void createDateDialog() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(MainActivity.this,
+                android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener,
+                year, month, day);
+
+        dialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
     }
 
     @Override
