6813db7352134be6591d056dbc3c5da6237e1d09
IncurableCodeDreamer <kla.96.kn@gmail.com>
2018-10-25 16:14:34 +0200

    Add validator for edittexts

--- app/src/main/java/com/example/klaudia/medicalcenter/MyCustomApplication.java
+++ app/src/main/java/com/example/klaudia/medicalcenter/MyCustomApplication.java
@@ -1,4 +1,32 @@
 package com.example.klaudia.medicalcenter;
 
-public class MyCustomApplication {
+import android.app.Application;
+import android.content.res.Configuration;
+
+import com.facebook.stetho.Stetho;
+
+public class MyCustomApplication extends Application {
+    // Called when the application is starting, before any other application objects have been created.
+    // Overriding this method is totally optional!
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+        // Required initialization logic here!
+    }
+
+    // Called by the system when the device configuration changes while your component is running.
+    // Overriding this method is totally optional!
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    // This is called when the overall system is running low on memory,
+    // and would like actively running processes to tighten their belts.
+    // Overriding this method is totally optional!
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
 }
