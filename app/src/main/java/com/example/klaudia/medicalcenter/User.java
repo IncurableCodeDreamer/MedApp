cb246d042c592269cd5b4af2a5bc70f9d18ef366
IncurableCodeDreamer <kla.96.kn@gmail.com>
2018-11-22 18:40:28 +0100

    Add sex of user to db

--- app/src/main/java/com/example/klaudia/medicalcenter/User.java
+++ app/src/main/java/com/example/klaudia/medicalcenter/User.java
@@ -5,11 +5,12 @@ public class User {
     private String surname;
     private String email;
     private String birthDate;
+    private String sex;
     private boolean ifDonor;
     byte [] picture;
     private int age;
 
-    public User(String name, String surname, String email, String birthDate, boolean ifDonor, int age, byte [] picture) {
+    public User(String name, String surname, String email, String birthDate, String sex, boolean ifDonor, int age, byte [] picture) {
         this.name = name;
         this.surname = surname;
         this.email = email;
@@ -17,6 +18,7 @@ public class User {
         this.ifDonor = ifDonor;
         this.age = age;
         this.picture = picture;
+        this.sex = sex;
     }
 
     public User() {
@@ -41,6 +43,10 @@ public class User {
     }
 
     public byte[] getPicture() { return picture; }
+
+    public String getSex() { return sex; }
+
+    public void setSex(String sex) { this.sex = sex; }
 
     public void setPicture(byte[] picture) { this.picture = picture; }
 
@@ -77,16 +83,17 @@ public class User {
     public static final String AGE = "age";
     public static final String IFDONOR = "ifdonor";
     public static final String PICTURE = "picture";
+    public static final String SEX = "sex";
 
     public static final String CREATE_TABLE =
             "CREATE TABLE IF NOT EXISTS " + TABLE + " ( " +
                     ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     NAME + " TEXT, " +
                     SURNAME + " TEXT, " +
+                    SEX + " TEXT, " +
                     EMAIL + " TEXT, " +
                     BIRTHDATE + " TEXT, " +
                     IFDONOR + " REAL, " +
                     PICTURE + " BLOB, " +
                     AGE + " INTEGER " + "); ";
-
 }
