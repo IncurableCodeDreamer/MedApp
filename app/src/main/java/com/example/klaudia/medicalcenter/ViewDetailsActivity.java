43f953968c0bbeb747f3858ffc3df4061bd40001
IncurableCodeDreamer <kla.96.kn@gmail.com>
2018-12-07 17:03:33 +0100

    Add white icon to top menu

--- app/src/main/java/com/example/klaudia/medicalcenter/ViewDetailsActivity.java
+++ app/src/main/java/com/example/klaudia/medicalcenter/ViewDetailsActivity.java
@@ -14,7 +14,7 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.klaudia.medicalcenter.Model.PlaceDetails;
+import com.example.klaudia.medicalcenter.RetrofitModel.PlaceDetails;
 import com.example.klaudia.medicalcenter.Remote.IGoogleApiService;
 import com.squareup.picasso.Picasso;
 
