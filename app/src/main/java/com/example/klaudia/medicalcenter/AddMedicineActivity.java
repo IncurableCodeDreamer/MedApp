cff35389df3d07075e7a2e93da8b957947fb3ed4
IncurableCodeDreamer <kla.96.kn@gmail.com>
2018-12-10 17:08:52 +0100

    Add csv medicine list

--- app/src/main/java/com/example/klaudia/medicalcenter/AddMedicineActivity.java
+++ app/src/main/java/com/example/klaudia/medicalcenter/AddMedicineActivity.java
@@ -2,7 +2,6 @@ package com.example.klaudia.medicalcenter;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -15,14 +14,13 @@ import com.example.klaudia.medicalcenter.DatabaseModel.Medicine;
 import com.example.klaudia.medicalcenter.Helper.DatabaseHelper;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
-import com.mobsandgeeks.saripaar.annotation.Length;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
-import com.mobsandgeeks.saripaar.annotation.Pattern;
-import com.opencsv.CSVReader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -32,11 +30,8 @@ public class AddMedicineActivity extends AppCompatActivity implements Validator.
 
     @BindView(R.id.add_medicine_item_frequency)
     Spinner dropdown;
-    @NotEmpty(message = "Pole nie może pozostać puste")
-    @Length(min = 3, message = "Nazwa leku musi miec powyzej 3 liter")
-    @Pattern(sequence = 2, regex = "[a-zA-Z][a-zA-Z ]+", message = "Wprowadz dane w odpowiedniej formie")
     @BindView(R.id.add_medicine_item_name)
-    EditText add_medicine_item_name;
+    Spinner add_medicine_item_name;
     @NotEmpty(message = "Pole nie może pozostać puste")
     @BindView(R.id.add_medicine_item_amount)
     EditText add_medicine_item_amount;
@@ -44,7 +39,6 @@ public class AddMedicineActivity extends AppCompatActivity implements Validator.
     EditText add_medicine_item_add_info;
     @BindView(R.id.add_medicine_btn_activity)
     Button add_medicine_btn;
-    List myEntries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,35 +46,22 @@ public class AddMedicineActivity extends AppCompatActivity implements Validator.
         setContentView(R.layout.activity_add_medicine);
         setTitle(AddMedicineActivity.this.getTitle());
         ButterKnife.bind(this);
-        
-        List myEntries2;
+        List<String> tokens = new ArrayList<>();
 
         try {
-            CSVReader reader2 = new CSVReader(new FileReader("leki.csv"));
-            List<String[]> myEntries = reader2.readAll();
-
-            File csvfile = new File(Environment.getExternalStorageDirectory() + "/leki.csv");
-            CSVReader reader = new CSVReader(new FileReader(csvfile.getAbsolutePath()));
-            String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-                //myEntries.add(nextLine[1]);
-                // nextLine[] is an array of values from the line
-                //System.out.println(nextLine[0] + nextLine[1] + "etc...");
+            InputStream is = getResources().openRawResource(R.raw.rejestrproduktow);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String line;
+            while((line = reader.readLine()) != null){
+               tokens.add(line);
             }
         } catch (Exception e) {
             e.printStackTrace();
             Toast.makeText(this, "The specified file was not found", Toast.LENGTH_SHORT).show();
         }
 
-        CSVReader reader = null;
-        try {
-            reader = new CSVReader(new FileReader("leki.csv"));
-            myEntries2 = reader.readAll();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        ArrayAdapter<String> adapterMedicine = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, tokens);
+        add_medicine_item_name.setAdapter(adapterMedicine);
 
         String[] items = new String[]{"Dziennie", "Tygodniowo", "Miesięcznie"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, items);
@@ -98,7 +79,7 @@ public class AddMedicineActivity extends AppCompatActivity implements Validator.
 
     private void addMedicine() {
         Medicine medicine = new Medicine();
-        medicine.setName(add_medicine_item_name.getText().toString());
+        medicine.setName(add_medicine_item_name.getSelectedItem().toString());
         medicine.setFrequency(dropdown.getSelectedItem().toString());
         medicine.setAddInfo(add_medicine_item_add_info.getText().toString());
         medicine.setAmount(add_medicine_item_amount.getText().toString());
